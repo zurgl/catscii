bb3d7fd62fb4a00e8043a7634e41b6b81fe46b72
yacine <elayar.yacine@gmail.com>
2023-04-14 10:43:30 +0000

    add sentry

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,9 @@ async fn main() {
         .with(filter)
         .init();
 
-    let app = Router::new().route("/", get(root_get));
+    let app = Router::new()
+        .route("/", get(root_get))
+        .route("/panic", get(|| async { panic!("This is a test panic") }));
 
     let addr = "0.0.0.0:8080".parse().unwrap();
     info!("Listening on {addr}");
