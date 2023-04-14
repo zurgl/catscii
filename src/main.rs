eb081a53f4bb30b984bbbb15d4fca1ffbcbc6b2a
yacine <elayar.yacine@gmail.com>
2023-04-14 10:14:20 +0000

    Add crash reporting with Sentry

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,15 @@ use tracing_subscriber::{filter::Targets, layer::SubscriberExt, util::Subscriber
 
 #[tokio::main]
 async fn main() {
+    // at the top of main
+    let _guard = sentry::init((
+        std::env::var("SENTRY_DSN").expect("$SENTRY_DSN must be set"),
+        sentry::ClientOptions {
+            release: sentry::release_name!(),
+            ..Default::default()
+        },
+    ));
+
     let filter = Targets::from_str(std::env::var("RUST_LOG").as_deref().unwrap_or("info"))
         .expect("RUST_LOG should be a valid tracing filter");
     tracing_subscriber::fmt()
