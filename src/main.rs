05c28aacec08f6471c89786b510b343fd23f523e
yacine <elayar.yacine@gmail.com>
2023-04-14 11:13:48 +0000

    add honeycomb

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,31 @@
 use std::str::FromStr;
 
+use opentelemetry::{
+    global,
+    trace::{get_active_span, FutureExt, Span, Status, TraceContextExt, Tracer},
+    Context, KeyValue,
+};
+
 use axum::{
     body::BoxBody,
-    http::header,
+    http::{header, HeaderMap},
     response::{IntoResponse, Response},
     routing::get,
     Router,
 };
 use reqwest::StatusCode;
-use serde::Deserialize;
 use tracing::{info, Level};
 use tracing_subscriber::{filter::Targets, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() {
     // at the top of main
-    let _guard = sentry::init((
-        std::env::var("SENTRY_DSN").expect("$SENTRY_DSN must be set"),
-        sentry::ClientOptions {
-            release: sentry::release_name!(),
-            ..Default::default()
-        },
-    ));
+    let (_honeyguard, _tracer) = opentelemetry_honeycomb::new_pipeline(
+        std::env::var("HONEYCOMB_API_KEY").expect("$HONEYCOMB_API_KEY should be set"),
+        "catscii".into(),
+    )
+    .install()
+    .unwrap();
 
     let filter = Targets::from_str(std::env::var("RUST_LOG").as_deref().unwrap_or("info"))
         .expect("RUST_LOG should be a valid tracing filter");
@@ -44,8 +48,31 @@ async fn main() {
         .unwrap();
 }
 
-async fn root_get() -> Response<BoxBody> {
-    match get_cat_ascii_art().await {
+async fn root_get(headers: HeaderMap) -> Response<BoxBody> {
+    let tracer = global::tracer("");
+    let mut span = tracer.start("root_get");
+    span.set_attribute(KeyValue::new(
+        "user_agent",
+        headers
+            .get(header::USER_AGENT)
+            .map(|h| h.to_str().unwrap_or_default().to_owned())
+            .unwrap_or_default(),
+    ));
+
+    root_get_inner()
+        .with_context(Context::current_with_span(span))
+        .await
+}
+
+async fn root_get_inner() -> Response<BoxBody> {
+    let tracer = global::tracer("");
+
+    match get_cat_ascii_art()
+        .with_context(Context::current_with_span(
+            tracer.start("get_cat_ascii_art"),
+        ))
+        .await
+    {
         Ok(art) => (
             StatusCode::OK,
             [(header::CONTENT_TYPE, "text/html; charset=utf-8")],
@@ -53,19 +80,59 @@ async fn root_get() -> Response<BoxBody> {
         )
             .into_response(),
         Err(e) => {
-            println!("Something went wrong: {e}");
+            get_active_span(|span| {
+                span.set_status(Status::Error {
+                    description: format!("{e}").into(),
+                })
+            });
             (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong").into_response()
         }
     }
 }
+
 async fn get_cat_ascii_art() -> color_eyre::Result<String> {
-    #[derive(Deserialize)]
+    let tracer = global::tracer("");
+
+    let client = reqwest::Client::default();
+
+    let image_url = get_cat_image_url(&client)
+        .with_context(Context::current_with_span(
+            tracer.start("get_cat_image_url"),
+        ))
+        .await?;
+
+    let image_bytes = download_file(&client, &image_url)
+        .with_context(Context::current_with_span(tracer.start("download_file")))
+        .await?;
+
+    let image = tracer.in_span("image::load_from_memory", |cx| {
+        let img = image::load_from_memory(&image_bytes)?;
+        cx.span()
+            .set_attribute(KeyValue::new("width", img.width() as i64));
+        cx.span()
+            .set_attribute(KeyValue::new("height", img.height() as i64));
+        Ok::<_, color_eyre::eyre::Report>(img)
+    })?;
+
+    let ascii_art = tracer.in_span("artem::convert", |_cx| {
+        artem::convert(
+            image,
+            artem::options::OptionBuilder::new()
+                .target(artem::options::TargetType::HtmlFile(true, true))
+                .build(),
+        )
+    });
+
+    Ok(ascii_art)
+}
+
+async fn get_cat_image_url(client: &reqwest::Client) -> color_eyre::Result<String> {
+    #[derive(serde::Deserialize)]
     struct CatImage {
         url: String,
     }
 
     let api_url = "https://api.thecatapi.com/v1/images/search";
-    let client = reqwest::Client::default();
 
     let image = client
         .get(api_url)
@@ -76,22 +143,16 @@ async fn get_cat_ascii_art() -> color_eyre::Result<String> {
         .await?
         .pop()
         .ok_or_else(|| color_eyre::eyre::eyre!("The Cat API returned no images"))?;
+    Ok(image.url)
+}
 
-    let image_bytes = client
-        .get(image.url)
+async fn download_file(client: &reqwest::Client, url: &str) -> color_eyre::Result<Vec<u8>> {
+    let bytes = client
+        .get(url)
         .send()
         .await?
         .error_for_status()?
         .bytes()
         .await?;
-
-    let image = image::load_from_memory(&image_bytes)?;
-    let ascii_art = artem::convert(
-        image,
-        artem::options::OptionBuilder::new()
-            .target(artem::options::TargetType::HtmlFile(true, true))
-            .build(),
-    );
-
-    Ok(ascii_art)
+    Ok(bytes.to_vec())
 }
