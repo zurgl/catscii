0af054efe6f985fd7518a9af5322e0ad3430c091
yacine <elayar.yacine@gmail.com>
2023-04-14 09:31:25 +0000

    ascii art cat on term

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,41 @@
+//use pretty_hex::PrettyHex;
+use serde::Deserialize;
+
 #[tokio::main]
 async fn main() {
-    let res = reqwest::get("https://api.thecatapi.com/v1/images/search")
-        .await
-        .unwrap();
-    println!("Status: {}", res.status());
-    let body = res.text().await.unwrap();
-    println!("Body: {}", body);
+    let art = get_cat_ascii_art().await.unwrap();
+    println!("{art}");
+}
+
+async fn get_cat_ascii_art() -> color_eyre::Result<String> {
+    #[derive(Deserialize)]
+    struct CatImage {
+        url: String,
+    }
+
+    let api_url = "https://api.thecatapi.com/v1/images/search";
+    let client = reqwest::Client::default();
+
+    let image = client
+        .get(api_url)
+        .send()
+        .await?
+        .error_for_status()?
+        .json::<Vec<CatImage>>()
+        .await?
+        .pop()
+        .ok_or_else(|| color_eyre::eyre::eyre!("The Cat API returned no images"))?;
+
+    let image_bytes = client
+        .get(image.url)
+        .send()
+        .await?
+        .error_for_status()?
+        .bytes()
+        .await?;
+
+    let image = image::load_from_memory(&image_bytes)?;
+    let ascii_art = artem::convert(image, artem::options::OptionBuilder::new().build());
+
+    Ok(ascii_art)
 }
